1a56077087861e1337aeb8e026b7f0e9bd035b98
pawel <pawel@pdebian.pzieba.pl>
2012-11-03 00:46:46 +0100

    -- kręciołka podczas czekania na odp. podczas nauki

--- urls.py
+++ urls.py
@@ -20,7 +20,7 @@ urlpatterns = patterns('',
     (r'^lesson/(\d+)', 'words.views.manage.lesson_details'),
     
     (r'^question/new$', 'words.views.manage.question_new_tile'),
-    (r'^question/(\d+)/details$', 'words.views.manage.question_details_tile'),
+    (r'^question/(\d+)/details$', 'words.views.manage.question_details'),
     
     (r'^lesson/learn$', 'words.views.learn.learn'),
     (r'^lesson/learn/lesson/(?P<lesson_id>\d+)$', 'words.views.learn.learn', {}, "learn_lesson"),
@@ -29,6 +29,8 @@ urlpatterns = patterns('',
     (r'^lesson/learn/ask$', 'words.views.learn.ask_question'),
     (r'^lesson/learn/check$', 'words.views.learn.check'),
     (r'^lesson/learn/score/(\d)$', 'words.views.learn.score'),
+    
+    (r'^tile/question/(\d+)/details$', 'words.views.manage.question_details_tile'),
     
     (r'^$', 'words.views.manage.index'),
 )
